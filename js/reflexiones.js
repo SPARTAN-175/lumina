4e6ee992f7621dbaede5a9a4be0df23a008f97b6
SPARTAN-175 <avengers_0077@hotmail.com>
2026-06-15 12:34:19 -0600

    Update reflexiones.js

--- js/reflexiones.js
+++ js/reflexiones.js
@@ -1,1 +1,39 @@
+/* ======================================
+   REFLEXIÓN DEL DÍA
+====================================== */
 
+window.cargarReflexion = async function(){
+
+    const referencia =
+        doc(db,"reflexion_actual","actual");
+
+    const documento =
+        await getDoc(referencia);
+
+    if(documento.exists()){
+
+        const datos = documento.data();
+
+        document.getElementById("versiculo")
+        .textContent = datos.versiculo;
+
+        document.getElementById("texto")
+        .textContent = datos.texto;
+
+        document.getElementById("pregunta")
+        .textContent =
+        "❓ " + datos.pregunta;
+
+        document.getElementById(
+            "versiculoFijo"
+        ).textContent =
+        datos.versiculo;
+
+        document.getElementById(
+            "preguntaFija"
+        ).textContent =
+        "❓ " + datos.pregunta;
+
+    }
+
+};
