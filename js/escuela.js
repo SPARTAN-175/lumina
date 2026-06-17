a60cc4b999a84f6f1142462dc840479ccf1cd042
SPARTAN-175 <avengers_0077@hotmail.com>
2026-06-17 13:18:50 -0600

    Update escuela.js

--- js/escuela.js
+++ js/escuela.js
@@ -1,3 +1,36 @@
-console.log(
-"Escuela cargada correctamente"
-);
+let temasAbiertos = false;
+
+window.toggleTemas = function(){
+
+    const contenedor =
+    document.getElementById(
+        "temasOcultos"
+    );
+
+    const boton =
+    document.getElementById(
+        "btnVerTemas"
+    );
+
+    temasAbiertos =
+    !temasAbiertos;
+
+    if(temasAbiertos){
+
+        contenedor.style.display =
+        "block";
+
+        boton.textContent =
+        "Ocultar temas";
+
+    }else{
+
+        contenedor.style.display =
+        "none";
+
+        boton.textContent =
+        "Ver todos los temas";
+
+    }
+
+};
